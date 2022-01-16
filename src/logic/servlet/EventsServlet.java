1acdd09a9337504cec3977def35c8e6ddd3785a9
Danilo D'Amico <45599533+DaniloDamico@users.noreply.github.com>
2022-01-16 22:25:10 +0100

    addressed remaining //TODO

--- src/logic/servlet/EventsServlet.java
+++ src/logic/servlet/EventsServlet.java
@@ -19,14 +19,14 @@ public class EventsServlet extends HttpServlet {
      * Default constructor.
      */
     public EventsServlet() {
-        // TODO Auto-generated constructor stub
+        // stub
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+        // stub
     }
 
 }
